6a1faf5eb3a07ea4bef1ddb68fc6863391cba482
りもこ <128880711+limonene213-aktk@users.noreply.github.com>
2024-06-11 12:06:13 +0900

    Create html-echo.php

---
+++ Chapter3/3-17-18-19/html-echo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+<body>
+    <p>
+        <?php
+        //変数を入れてみる
+        $moji = $_POST["input"];
+        $moji2 = $_POST["input2"];
+
+        echo $moji."\n";
+        echo $moji2;
+        ?>
+    </p>
+</body>
+</html>
